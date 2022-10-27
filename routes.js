1d435805769643eb4c68d0cf8a2f090dfd4a1d8e
‘Tarold’ <ltdecorer@gmail.com>
2022-10-27 16:37:01 +0300

    Add swagger

--- routes.js
+++ routes.js
@@ -1,7 +1,29 @@
 const User = require("./models/UserModels"),
-  userController = require("./controllers/UserController.js");
+  userController = require("./controllers/userController.js");
 
 module.exports = function (app) {
+  /**
+   *@swagger
+   *  /user:
+   *     get:
+   *       tags: [User]
+   *       description: Get all users
+   *       responses:
+   *         200:
+   *           description: "success"
+   *           content:
+   *             application/json:
+   *               schema:
+   *                 type: array
+   *                 items:
+   *                   properties:
+   *                     name:
+   *                       type: string
+   *                       example: yaroslav
+   *                     age:
+   *                       type: integer
+   *                       example: 18
+   */
   app.get("/user", userController.getListOfUsers);
   app.get("/user/:name", userController.getUsersByName);
   app.post("/user", userController.addUser);
