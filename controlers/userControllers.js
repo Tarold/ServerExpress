f738988dd29a196c19e93735712c5b69858fbe06
‘Tarold’ <ltdecorer@gmail.com>
2022-11-24 14:22:58 +0200

    Added aggregate, findOne, $or, $regex exercise

--- controlers/userControllers.js
+++ controlers/userControllers.js
@@ -117,3 +117,111 @@ exports.getHeightsUser = (req, res) => {
     .sort({ name: 1 });
   // .limit(1);
 };
+
+exports.addFieldSex = (req, res) => {
+  user.updateMany(
+    {},
+    {
+      $set: { sex: 'male' },
+    },
+    (err, user) => {
+      if (err) res.status(404).send(err);
+      res.status(200).json({
+        message: 'Updated',
+      });
+    }
+  );
+};
+
+//Чому працюють тільки одинакові імена
+
+/*
+13) Додати поле стать в бд, якщо його не існує. За допомогою функції агрегації https://www.mongodb.com/docs/manual/reference/operator/aggregation/avg/ реалізувати отримання середнього зросту жінок і чоловіків
+
+Відповідь у вигляді
+
+[
+
+    {
+
+        "_id": "women",
+
+        "avg": "170"
+
+    },
+
+    {
+
+        "_id": "men",
+
+        "avg": "178"
+
+    }
+
+]
+*/
+
+exports.getUsersAvg = function (req, res) {
+  user.aggregate(
+    [
+      {
+        $group: {
+          _id: '$sex',
+          avg: { $avg: '$height' },
+        },
+      },
+    ],
+    (err, users) => {
+      if (err) res.send(err);
+      res.json(users);
+    }
+  );
+};
+
+//як працює синтаксис find
+// sort задає філтр для findOne
+/*10) Показати найcтаршого користувача, використати функцію findOne(). Додати перевірку, щоб поле зріст існувало в документі */
+exports.findOneOldest = (req, res) => {
+  user
+    .findOne(
+      {
+        height: { $exists: true },
+      },
+      (err, users) => {
+        if (err) res.send(err);
+        res.json(users);
+      }
+    )
+    .sort({ age: -1 });
+};
+
+//req i res яка різниця
+/*1) Видалити документи в яких порожнє АБО ім'я, АБО вік (використайте $or)*/
+exports.removeEmptyNameOrAge = (req, res) => {
+  user.remove(
+    {
+      $or: [
+        { name: { $exists: false } },
+        { name: '' },
+        { age: { $exists: false } },
+        { age: '' },
+      ],
+    },
+    (err, users) => {
+      if (err) res.send(err);
+      res.status(200).json({ message: 'Successful' });
+    }
+  );
+};
+
+//8) Показати корстувачів ім'я яких починається з заданої користувачем літери
+
+exports.findUserByLetter = (req, res) => {
+  user.find(
+    { name: { $regex: '^' + req.params.letter[0], $options: 'i' } },
+    (err, users) => {
+      if (err) res.send(err);
+      res.status(200).json({ message: users });
+    }
+  );
+};
--- routes.js
+++ routes.js
@@ -24,13 +24,20 @@ module.exports = function (app) {
    *                       type: integer
    *                       example: 18
    */
+  app.delete(
+    '/user/remove-empty-name-age',
+    userController.removeEmptyNameOrAge
+  );
+  app.get('/user/old/', userController.findOneOldest);
   app.get('/user', userController.getListOfUsers);
+  app.get('/user/find/:letter', userController.findUserByLetter);
   app.delete('/user/remove/doc', userController.removeEmpty);
   app.get('/user-height', userController.getHeightsUser);
-  app.get('/user/:name', userController.getUsersByName);
   app.post('/user', userController.addUser);
+  app.put('/add-height', userController.addFieldHeight);
+  app.get('/user/getavg/', userController.getUsersAvg);
+  app.get('/user/:name', userController.getUsersByName);
   app.delete('/user/:userId', userController.removeUser);
   app.put('/user/:userId', userController.updateUser);
   app.get('/user/user-by-age/:from/:to', userController.getUsersByAge);
-  app.put('/add-height', userController.addFieldHeight);
 };
